488bedeb20654d59556ac404379956e3dcea451b
cheshirrrr <jmnmnck@gmail.com>
2021-12-23 15:06:19 +0100

    added more files

--- src/main/java/com/dmkir/Main.java
+++ src/main/java/com/dmkir/Main.java
@@ -1,7 +1,15 @@
 package com.dmkir;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("done!");
+
+        for (int i = 0; i < 600; i++) {
+            Files.copy(Paths.get("test/file1"), Paths.get("test", "filecopy" + i));
+        }
     }
 }
