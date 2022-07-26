d7162dfc8c82ad140cd3fa9290bd82fef275c8cc
admin <admin@TeamCity>
2022-07-26 14:55:57 +0200

    TeamCity change in 'CloudFront Check' project: 'ImageBuilder Test' build configuration settings were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -273,6 +273,7 @@ object ImageBuilderTest : BuildType({
         step {
             name = "Image builder step"
             type = "awsImageBuilder"
+            enabled = false
             param("awsSessionDuration", "60")
             param("awsConnectionId", "PROJECT_EXT_14")
             param("cloud.aws.imagebuilder.base-ami", "ami-0339455b66e82a8ca")
